b991504d4fbf8b6969afb81595e2bc5e98ce8db2
EmperorJack <emperorpurvis@gmail.com>
2017-03-24 15:30:10 +1300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,138 @@
+// L-System Generator
+// Jack Purvis
+
+// System variables
+var system;
+var sentence;
+
+// Elements
+var textContainer, canvas;
+
+// Buttons
+var showTextButton, showCanvasButton, generateButton, resetButton;
+
+function setup() {
+  setupDOM();
+  setupSystem();
+}
+
+function setupSystem() {
+  system = getSystem(0);
+
+  // Grammar setup
+  sentence = system.axiom;
+
+  // Initial output
+  createP(system.axiom).parent(textContainer);
+  drawSentence();
+}
+
+function generate() {
+  var nextSentence = "";
+
+  // For each character
+  for (var i = 0; i < sentence.length; i++) {
+    var current = sentence.charAt(i);
+
+    var matchedRule = false;
+
+    // For each rule
+    for (var r = 0; r < system.rules.length; r++) {
+      var rule = system.rules[r];
+
+      // Matched
+      if (current == rule.input) {
+        nextSentence += rule.output;
+        matchedRule = true;
+        break;
+      }
+    }
+
+    // Unmatched
+    if (!matchedRule) {
+      nextSentence += current;
+    }
+  }
+
+  sentence = nextSentence;
+  createP(sentence).parent(textContainer);
+  drawSentence();
+}
+
+function drawSentence() {
+  background(200);
+
+  fill(0);
+  stroke(0);
+  strokeWeight(2);
+
+  translate(system.startX, system.startY);
+
+  rotate(radians(system.startAngle));
+
+  // For each character
+  for (var c = 0; c < sentence.length; c++) {
+    var current = sentence.charAt(c);
+
+    // For each instruction
+    for (var i = 0; i < system.instructions.length; i++) {
+      var instruction = system.instructions[i];
+
+      if (current == instruction.char) {
+        instruction.action(system);
+        break;
+      }
+    }
+  }
+}
+
+function setupDOM() {
+  textContainer = createDiv("");
+  textContainer.position(10, 50);
+
+  showTextButton = createButton('Show Text');
+  showTextButton.position(10, 10);
+  showTextButton.mousePressed(showText);
+  showTextButton.hide();
+
+  showCanvasButton = createButton('Show Canvas');
+  showCanvasButton.position(90, 10);
+  showCanvasButton.mousePressed(showCanvas);
+
+  generateButton = createButton("Generate");
+  generateButton.position(190, 10);
+  generateButton.mousePressed(generate);
+
+  resetButton = createButton("Reset");
+  resetButton.position(270, 10);
+  resetButton.mousePressed(reset);
+
+  canvas = createCanvas(600, 600);
+  canvas.position(10, 50);
+  canvas.hide();
+}
+
+function showText() {
+  showTextButton.hide();
+  canvas.hide();
+  textContainer.show();
+  showCanvasButton.show();
+}
+
+function showCanvas() {
+  showCanvasButton.hide();
+  textContainer.hide();
+  canvas.show();
+  showTextButton.show();
+}
+
+function reset() {
+  textContainer.remove();
+  textContainer = createDiv("");
+  textContainer.position(10, 50);
+  showText();
+
+  sentence = system.axiom;
+  createP(system.axiom).parent(textContainer);
+  drawSentence();
+}
---
+++ systems.js
@@ -0,0 +1,206 @@
+function getSystem(index) {
+  return [
+
+  // Pythagoras tree
+  {
+    axiom: "0",
+    rules: [
+      {
+        input: "0",
+        output: "1[0]0"
+      },
+      {
+        input: "1",
+        output: "11"
+      }
+    ],
+    startX: width / 2,
+    startY: height,
+    startAngle: 0,
+    lineLength: 10,
+    rotation: 90,
+    instructions: [
+      {
+        char: "0",
+        action: function() {
+          line(0, 0, 0, -system.lineLength);
+          translate(0, -system.lineLength);
+        }
+      },
+      {
+        char: "1",
+        action: function() {
+          line(0, 0, 0, -system.lineLength);
+          translate(0, -system.lineLength);
+        }
+      },
+      {
+        char: "[",
+        action: function() {
+          push();
+          rotate(-radians(system.rotation));
+        }
+      },
+      {
+        char: "]",
+        action: function() {
+          pop();
+          rotate(radians(system.rotation));
+        }
+      }
+    ]
+  },
+
+  // Fractal Plant
+  {
+    axiom: "F",
+    rules: [
+      {
+        input:  "F",
+        output: "FF+[+F-F-F]-[-F+F+F]"
+      }
+    ],
+    startX: width / 2,
+    startY: height,
+    startAngle: 0,
+    lineLength: 10,
+    rotation: -15,
+    instructions: [
+      {
+        char: "F",
+        action: function() {
+          line(0, 0, 0, -system.lineLength);
+          translate(0, -system.lineLength);
+        }
+      },
+      {
+        char: "G",
+        action: function() {
+          line(0, 0, 0, -system.lineLength);
+          translate(0, -system.lineLength);
+        }
+      },
+      {
+        char: "+",
+        action: function() {
+          rotate(-radians(system.rotation));
+        }
+      },
+      {
+        char: "-",
+        action: function() {
+          rotate(radians(system.rotation));
+        }
+      },
+      {
+        char: "[",
+        action: function() {
+          push();
+        }
+      },
+      {
+        char: "]",
+        action: function() {
+          pop();
+        }
+      }
+    ]
+  },
+
+  // Sierpinski triangle
+  {
+    axiom: "F-G-G",
+    rules: [
+      {
+        input:  "F",
+        output: "F-G+F+G-F"
+      },
+      {
+        input:  "G",
+        output: "GG"
+      }
+    ],
+    startX: 0,
+    startY: height,
+    startAngle: 30,
+    lineLength: 10,
+    rotation: 120,
+    instructions: [
+      {
+        char: "F",
+        action: function() {
+          line(0, 0, 0, -system.lineLength);
+          translate(0, -system.lineLength);
+        }
+      },
+      {
+        char: "G",
+        action: function() {
+          line(0, 0, 0, -system.lineLength);
+          translate(0, -system.lineLength);
+        }
+      },
+      {
+        char: "+",
+        action: function() {
+          rotate(-radians(system.rotation));
+        }
+      },
+      {
+        char: "-",
+        action: function() {
+          rotate(radians(system.rotation));
+        }
+      }
+    ]
+  },
+
+  // Dragon curve
+  {
+    axiom: "FX",
+    rules: [
+      {
+        input:  "X",
+        output: "X+YGF+"
+      },
+      {
+        input:  "Y",
+        output: "-FX-Y"
+      }
+    ],
+    startX: width / 2,
+    startY: height / 2,
+    startAngle: 0,
+    lineLength: 5,
+    rotation: 90,
+    instructions: [
+      {
+        char: "F",
+        action: function() {
+          line(0, 0, 0, -system.lineLength);
+          translate(0, -system.lineLength);
+        }
+      },
+      {
+        char: "+",
+        action: function() {
+          rotate(radians(system.rotation));
+        }
+      },
+      {
+        char: "-",
+        action: function() {
+          rotate(radians(-system.rotation));
+        }
+      },
+      {
+        char: "G",
+        action: function() {
+          stroke(random(255), 0, 0);
+        }
+      }
+    ]
+  }
+
+  ][index]
+}
